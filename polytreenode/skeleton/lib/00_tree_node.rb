2188df088c95cfe20dce2113367da458adf93bb4
yujie zhu <yujiezhu718@gmail.com>
2018-02-16 11:41:48 -0800

    added parent and children methods

--- polytreenode/skeleton/lib/00_tree_node.rb
+++ polytreenode/skeleton/lib/00_tree_node.rb
@@ -1,3 +1,20 @@
 class PolyTreeNode
+  attr_reader :children, :value, :parent
+  def initialize(value)
+    @parent = nil
+    @children = []
+    @value = value
+  end
+
+  def parent=(parent)
+    @parent = parent
+    parent.children << self
+  end
+
+  def inspect
+    value
+  end
+
+
 
 end
