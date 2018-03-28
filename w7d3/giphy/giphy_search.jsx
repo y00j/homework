14937720aefbb379cab0b39b47ec9047034a6e99
Yujie Zhu <yujiezhu718@gmail.com>
2018-03-28 13:06:20 -0700

    w7d3 hw

---
+++ w7d3/giphy/giphy_search.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import Root from './components/root';
+// import {fetchSearchGiphys} from './util/api_util';
+import {fetchSearchGiphys} from './actions/giphy_actions';
+import {receiveSearchGiphys} from './actions/giphy_actions';
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+
+
+
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root store={store} />, root);
+
+});
